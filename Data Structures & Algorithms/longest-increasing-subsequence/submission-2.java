af73b66e35ec35d1ecd9579233969effdd994447
shubham612 <53951171+shubham612@users.noreply.github.com>
2026-09-16 17:35:37 +0530

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+       int n = nums.length;
+       int dp[][] = new int[n+1][n+1];
+
+       for(int i=n-1;i>=0;i--){
+         for(int j=i-1;j>=-1;j--){
+            int LIS = dp[i+1][j+1];
+            if(j==-1 || nums[i]>nums[j]){
+                LIS = Math.max(LIS,1+dp[i+1][i+1]);
+            }
+            dp[i][j+1] = LIS;
+         }
+       }
+
+       return dp[0][0]; 
+    }
+}
