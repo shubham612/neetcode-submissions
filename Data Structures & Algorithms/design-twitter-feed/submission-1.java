28f9cde6f2d1a1b2019469795415369bb6ef20bd
shubham612 <53951171+shubham612@users.noreply.github.com>
2026-09-18 06:23:18 +0530

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.java
@@ -0,0 +1,88 @@
+class Twitter {
+    class Tweet {
+        int tweetId;
+        int time;
+        Tweet(int tweetId, int time) {
+            this.tweetId = tweetId;
+            this.time = time;
+        }
+    }
+
+    private Map<Integer, Set<Integer>> followMap;
+    private Map<Integer, List<Tweet>> userTweets; 
+    private int timestamp;
+    
+    public Twitter() {
+        followMap = new HashMap<>();
+        userTweets = new HashMap<>();
+        timestamp = 0;        
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        Tweet newTweet = new Tweet(tweetId,timestamp++);
+        List<Tweet> tweets;
+        if(userTweets.containsKey(userId)){
+            tweets = userTweets.get(userId);
+        }else{
+           tweets = new ArrayList<>();
+        }
+        tweets.add(newTweet);
+        userTweets.put(userId,tweets);
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> feed = new ArrayList<>();
+        PriorityQueue<Tweet> recentTweets = new PriorityQueue<>((a,b)->(a.time)-(b.time));
+
+         Set<Integer> peopleToCheck = new HashSet<>();
+         peopleToCheck.add(userId); // always include self
+         if (followMap.containsKey(userId)) {
+              peopleToCheck.addAll(followMap.get(userId));
+         }
+
+         for(Integer user : peopleToCheck){
+            List<Tweet> tweets = userTweets.get(user);
+            if(tweets!=null && tweets.size()>0){
+                for(Tweet tweet : tweets){
+                      recentTweets.offer(tweet); 
+                     if(recentTweets.size()>10){
+                         recentTweets.poll();
+                      }
+                }
+            }
+           
+         }
+
+         while(!recentTweets.isEmpty()){
+           feed.add(recentTweets.peek().tweetId);
+           recentTweets.poll();
+         }
+
+         Collections.reverse(feed);
+         return feed;    
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        Set<Integer> following;
+        if(followMap.containsKey(followerId)){
+           following = followMap.get(followerId);
+        }else{
+            following = new HashSet<Integer>();
+        }
+        following.add(followeeId);
+        followMap.put(followerId,following);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        Set<Integer> following;
+        if(followMap.containsKey(followerId)){
+           following = followMap.get(followerId);
+           following.remove(followeeId);
+           if(following.size()>0){
+             followMap.put(followerId,following);
+           }else{
+             followMap.remove(followerId);
+           }  
+        }
+    }
+}
